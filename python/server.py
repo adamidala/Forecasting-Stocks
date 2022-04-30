0c94634798e622ef0840284f69191baf72079afe
tembos <jeanlegrais@yahoo.fr>
2022-04-30 19:04:59 +0200

    api ok

--- python/server.py
+++ python/server.py
@@ -1,9 +1,28 @@
 from flask import Flask
+import requests
 
 app = Flask(__name__)
 
+#GET DATA STOCK
+stock = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY&symbol=IBM&apikey=DQJ092TSFPDF9EKZ")
+try:
+    stock_parsed = stock.json()
+except:
+    pass
+
 @app.route('/')
 def index():
-    return 'jean et adam'
+    return "en cours de dev"
+
+
+@app.route('/data')
+def index():
+    if stock_parsed:
+        return stock_parsed["Weekly Time Series"]
+    else:
+        return 'jean et adam'
+
+
+
 
 app.run(host='0.0.0.0', port=3000)
